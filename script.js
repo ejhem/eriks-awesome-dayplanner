00290d23f3c8387c006bbdc51ffc50e4088ea3a0
ejhem <113192777+ejhem@users.noreply.github.com>
2022-11-24 16:15:27 -0400

    trying to get my code to change colours

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//date and time inject to document head
+var currentTime = dayjs();
+
+
+//date and time inject to document head with dayjs, i added the time instead of just the date on an extra line in the document
 var today = dayjs();
 $("#currentDay").text(today.format("dddd, MMMM D"));
 
@@ -7,18 +10,38 @@ var timeUpdater = setInterval(function () {
   $("#currentTime").text(today.format("h:mm:ss a"));
 });
 
+//setting colours of time to load before rest
+
+function timeColour() {
+  $(".time-block").each(function() {
+    if (blockTime < currentTime) {
+      $(this).addClass(".past");
+    } else if (blockTime = currentTime) {
+      $(this).addClass(".present");
+    } else {
+      $(this).addClass(".future");
+    }
+  });
+}
 //functions to save and load data into the correct blocks
 $(saveData);
 
 function saveData() {
   $(".time-block").each(function() {
+    //below line is calling the id in (this) timeBlock being the number of hour
     var timeBlock = $(this).attr("id");
-    $("#" + timeBlock + " textarea").text(localStorage.getItem(dayjs().format("hr") + timeBlock));
+    //below line is calling to input the info into hour text area from local storage, # being the hour, then dayjs is adding what hour it was input to help js determine what is to be put
+    $("#" + timeBlock + " textarea").text(localStorage.getItem(dayjs().format("h mm ") + timeBlock));
   });
+// below is calling the next function
   $(".saveBtn").on("click", saveTask);
 }
 
+
 function saveTask(e) {
+  //making sure page does not reload
+  event.preventDefault();
   var hourId = $(this).parent().attr("id");
-  localStorage.setItem(dayjs().format("hr") + hourId, $("#" + hourId + " textarea").val());
+  //this is saving the array information to local storage
+  localStorage.setItem(dayjs().format("h mm ") + hourId, $("#" + hourId + " textarea").val());
 }
