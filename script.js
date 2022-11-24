de6a91121c0af093e14203e375309e17b3e46012
ejhem <113192777+ejhem@users.noreply.github.com>
2022-11-24 09:33:42 -0400

    added hours from 9-5 and date and time and some styling

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var today = dayjs();
+$("#currentDay").text(today.format("dddd, MMMM D"));
+
+var timeUpdater = setInterval(function () {
+  var today = dayjs();
+  $("#currentTime").text(today.format("h:mm:ss a"));
+});
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
